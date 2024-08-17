bd839601aa5a17d566cc3352741ef38561828278
Marco Castañeda <marco_pascual410@hotmail.com>
2024-08-17 14:07:11 -0600

    Radio improv

--- lib/presentation/screens/radio/radio.dart
+++ lib/presentation/screens/radio/radio.dart
@@ -1,5 +1,4 @@
 import 'dart:async';
-import 'dart:convert';
 import 'dart:math';
 
 import 'package:animation_wrappers/animation_wrappers.dart';
@@ -16,13 +15,13 @@ import 'package:icarm/config/services/notification_ui_service.dart';
 import 'package:icarm/presentation/components/loading_widget.dart';
 import 'package:icarm/presentation/providers/providers.dart';
 import 'package:icarm/presentation/screens/radio/comments.dart';
+import 'package:icarm/presentation/screens/radio/widgets/radio_stream.dart';
 import 'package:just_audio/just_audio.dart';
 import 'package:just_audio_background/just_audio_background.dart';
 import 'package:lottie/lottie.dart';
 
 import 'package:icarm/config/setting/style.dart';
 import 'package:sizer_pro/sizer.dart';
-import 'package:http/http.dart' as http;
 import '../../components/zcomponents.dart';
 
 class RadioPage extends ConsumerStatefulWidget {
@@ -42,6 +41,7 @@ class _RadioPageState extends ConsumerState<RadioPage>
   int connectionTrys = 0;
   String currentSong = "";
   final commnetKey = new GlobalKey();
+  final streamHandler = StreamHandler();
 
   FocusNode commentField = FocusNode();
   double scrollPosition = 0;
@@ -125,41 +125,6 @@ class _RadioPageState extends ConsumerState<RadioPage>
   }
 
   void onLoading() {}
-
-  Stream<String> _currentSongStream() async* {
-    final url = Uri.parse(
-        "https://api.zeno.fm/mounts/metadata/subscribe/lcdmqnfduyqvv");
-    final client = http.Client();
-
-    try {
-      final request = http.Request('GET', url);
-      final response = await client.send(request);
-
-      await for (var chunk in response.stream.transform(utf8.decoder)) {
-        final lines = chunk.split('\n');
-        for (var line in lines) {
-          if (line.startsWith('data:')) {
-            final jsonData = line.substring(5).trim();
-
-            if (jsonData.isNotEmpty) {
-              try {
-                final Map<String, dynamic> resp = json.decode(jsonData);
-                if (resp["streamTitle"] != null) {
-                  yield resp["streamTitle"];
-                }
-              } catch (e) {
-                yield* Stream.error("Error parsing JSON: $e");
-              }
-            }
-          }
-        }
-      }
-    } catch (e) {
-      yield* Stream.error("Request error: $e");
-    } finally {
-      client.close();
-    }
-  }
 
   void initState() {
     ref.read(radioServiceProvider).playerStateStream.listen((state) async {
@@ -281,6 +246,8 @@ class _RadioPageState extends ConsumerState<RadioPage>
           genre: 'Religious',
           artUri: Uri.parse(
               'https://zeno.fm/_next/image/?url=https%3A%2F%2Fimages.zeno.fm%2FoU_QTjtJrboK2rm3nPb8NiKuieHzoQuYg06OF-85A8U%2Frs%3Afit%3A240%3A240%2Fg%3Ace%3A0%3A0%2FaHR0cHM6Ly9zdHJlYW0tdG9vbHMuemVub21lZGlhLmNvbS9jb250ZW50L3N0YXRpb25zLzJmNjE2OTI2LTkzOGQtNDNmZC1iYjBiLTBiMDM0M2ExMmFhMS9pbWFnZS8_dXBkYXRlZD0xNzE5NDYwNDEyMDAw.webp&w=3840&q=100'),
+          displayDescription: "Radio en vivo de la iglesia A&R Morelia",
+          displayTitle: "Radio En Vivo | Amor & Restauración Morelia",
         ),
       );
       //await audioSource.clearCache();
@@ -369,6 +336,7 @@ class _RadioPageState extends ConsumerState<RadioPage>
   Future<void> stopRadio() async {
     await audioPlayer.stop();
     ref.read(radioisPlayingProvider.notifier).update((state) => false);
+    streamHandler.stopStream();
   }
 
   void scrollTop() {
@@ -487,7 +455,8 @@ class _RadioPageState extends ConsumerState<RadioPage>
                                           height: 15,
                                         ),
                                         StreamBuilder<String>(
-                                            stream: _currentSongStream(),
+                                            stream: streamHandler
+                                                .currentSongStream(),
                                             builder: (context, snapshot) {
                                               if (!snapshot.hasData ||
                                                   snapshot.data == " - ") {
